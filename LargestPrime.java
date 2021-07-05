7bcffa8be42c2c68c76734d0fb249ee86d3189f7
berkay-karakus <72767236+berkay-karakus@users.noreply.github.com>
2021-07-05 13:01:05 +0300

    Update LargestPrime.java

--- LargestPrime.java
+++ LargestPrime.java
@@ -1,16 +1,16 @@
 
 public class LargestPrime {
-
 	public static int getLargestPrime(int number) {
 		  if (number < 2)
 			  return -1; // Invalid value
 		  
-		  	int largestPrime = 2;
-		  	while (number > 1) {
+		  	int largestPrime = 2; // possible largest prime factor initially
+		  	while (number > 1) { // search for factors until number goes to 1
+				// Find next prime
 		  		while (number % largestPrime != 0) {
 		  			largestPrime++;
 		  		} 
-		  		number /= largestPrime;
+		  		number /= largestPrime; // update number
 		  	}
 		  	
 		  	return largestPrime;
